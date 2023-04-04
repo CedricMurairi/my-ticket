296da1fe6424eaa602e9331c9805bf5affa0a1a5
ALUtech166 <s.tassiga@alustudent.com>
2023-04-04 19:33:29 +0200

    popup-update

--- lib/shared/ticket_widget.dart
+++ lib/shared/ticket_widget.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
-class TransportationTicket extends StatelessWidget {
-  const TransportationTicket({super.key});
+class TransportationTicketPopUp extends StatelessWidget {
+  const TransportationTicketPopUp({Key? key}) : super(key: key);
 
-  /*  final String ticketId;
+    /*  final String ticketId;
   final DateTime date;
   final String fromCity;
   final String toCity;
@@ -35,142 +35,122 @@ class TransportationTicket extends StatelessWidget {
   Widget build(BuildContext context) {
     // Generate QR code for the scan code
     final qrCode = QrImage(
-/*       data: ticketId,
- */
       data: 'TG1234',
       version: QrVersions.auto,
       size: 100.0,
     );
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Ticket'),
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
       ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(40.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // Ticket ID and date
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Ticket ID',
-                          style: TextStyle(
-                            fontSize: 14.0,
-                          )),
-                      SizedBox(
-                          height:
-                              10), // Add some space between the texts and the column
-
-                      Text('TG1234',
-                          style: TextStyle(
-                              fontSize: 16.0, fontWeight: FontWeight.bold))
-                    ],
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Date',
-                          style: TextStyle(
-                            fontSize: 14.0,
-                          )),
-                      SizedBox(
-                          height:
-                              10), // Add some space between the texts and the column
-
-                      Text('20-03-2023',
-                          style: TextStyle(
-                              fontSize: 16.0, fontWeight: FontWeight.bold)),
-                    ],
-                  )
-                ],
-              ),
-
-              SizedBox(height: 25.0),
-
-              // From and to destinations
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Kigali, Rwanda',
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Ticket ID and date
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Ticket ID',
                         style: TextStyle(
                           fontSize: 14.0,
-                        ),
-                      ),
-                      SizedBox(
-                          height:
-                              10), // Add some space between the texts and the column
-
-                      Text('KGL',
-                          style: TextStyle(
-                              fontSize: 25.0, fontWeight: FontWeight.bold)),
-                    ],
-                  ),
-                  Text(
-                    '-',
-                    style:
-                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Goma, DR Congo',
+                        )),
+                    SizedBox(height: 10), // Add some space between the texts and the column
+                    Text('TG1234',
+                        style: TextStyle(
+                            fontSize: 16.0, fontWeight: FontWeight.bold))
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Date',
                         style: TextStyle(
                           fontSize: 14.0,
-                        ),
-                      ),
-                      SizedBox(
-                          height:
-                              10), // Add some space between the texts and the column
-
-                      Text(
-                        'GMO',
+                        )),
+                    SizedBox(height: 10), // Add some space between the texts and the column
+                    Text('20-03-2023',
                         style: TextStyle(
-                            fontSize: 25.0, fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-
-              SizedBox(height: 20.0),
-              Divider(
-                thickness: 3, // Set the thickness of the line
-                color: Colors.black, // Set the color of the line
-              ),
-              SizedBox(height: 20.0),
-              Center(
-                child: Column(
+                            fontSize: 16.0, fontWeight: FontWeight.bold)),
+                  ],
+                )
+              ],
+            ),
+            SizedBox(height: 25.0),
+            // From and to destinations
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      'Passenger Name',
+                      'Kigali, Rwanda',
                       style: TextStyle(
                         fontSize: 14.0,
                       ),
                     ),
-                    SizedBox(
-                        height:
-                            10), // Add some space between the texts and the column
-                    Text(
-                      'Serge Tassiga',
-                      style: TextStyle(
-                          fontSize: 21.0, fontWeight: FontWeight.bold),
-                    ),
+                    SizedBox(height: 10), // Add some space between the texts and the column
+                    Text('KGL',
+                        style:
+                            TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold)),
                   ],
                 ),
+                Text(
+                  '-',
+                  style:
+                      TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Goma, DR Congo',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                      ),
+                    ),
+                    SizedBox(height: 10), // Add some space between the texts and the column
+                    Text(
+                      'GMO',
+                      style:
+                          TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            SizedBox(height: 20.0),
+            Divider(
+              thickness: 3, // Set the thickness of the line
+              color: Colors.black, // Set the color of the line
+            ),
+            SizedBox(height: 20.0),
+            Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Passenger Name',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                    ),
+                  ),
+                  SizedBox(height: 10), // Add some space between the texts and the column
+                  Text(
+                    'Serge Tassiga',
+                    style:
+                        TextStyle(fontSize: 21.0, fontWeight: FontWeight.bold),
+                  ),
+                ],
               ),
-              SizedBox(height: 20.0),
+SizedBox(height: 20.0),
               Divider(
                 thickness: 3, // Set the thickness of the line
                 color: Colors.black, // Set the color of the line
@@ -223,6 +203,6 @@ class TransportationTicket extends StatelessWidget {
           ),
         ),
       ),
-    );
+    
   }
 }
