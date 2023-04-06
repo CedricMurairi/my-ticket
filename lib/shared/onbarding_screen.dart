b8346540452e5981ce5dfc7b814f8925efaa9b08
CedricMurairi <murairicedric@gmail.com>
2023-04-06 11:03:54 +0200

    Changed 2d list to list of maps and added progress indicator to onboarding

--- lib/shared/onbarding_screen.dart
+++ lib/shared/onbarding_screen.dart
@@ -9,22 +9,28 @@ class Onboarding extends StatefulWidget {
 
 class _OnboardingState extends State<Onboarding> {
   int targetPage = 0;
-  final List<List<String>> onboardingData = [
-    [
-      'assets/images/welcome_icon.png',
-      'Welcome to Itike',
-      'Plan your trip with ease and buy bus tickets in advance, all from the comfort of your own device.'
-    ],
-    [
-      'assets/images/book_ticket.png',
-      'Buy Bus Tickets',
-      'Purchase bus tickets securely and easily with various payment methods, including Mobile Money.'
-    ],
-    [
-      'assets/images/real_time.png',
-      'Real-Time Information',
-      'Access real-time information on ticket availability and travel schedules. Find and buy bus tickets now!'
-    ],
+  final List<Map<String, String>> onboardingData = [
+    {
+      'image': 'assets/images/welcome_icon.png',
+      'title': 'Welcome to Itike',
+      'text':
+          'Plan your trip with ease and buy bus tickets in advance, all from the comfort of your own device.',
+      'action': 'Continue'
+    },
+    {
+      'image': 'assets/images/book_ticket.png',
+      'title': 'Buy Bus Tickets',
+      'text':
+          'Purchase bus tickets securely and easily with various payment methods, including Mobile Money.',
+      'action': 'Continue'
+    },
+    {
+      'image': 'assets/images/real_time.png',
+      'title': 'Real-Time Information',
+      'text':
+          'Access real-time information on ticket availability and travel schedules. Find and buy bus tickets now!',
+      'action': 'Book Your Ticket Now'
+    },
   ];
 
   @override
@@ -41,13 +47,13 @@ class _OnboardingState extends State<Onboarding> {
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 20.0),
               child: Image.asset(
-                onboardingData[targetPage][0],
+                onboardingData[targetPage]['image']!,
               ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 8.0),
               child: Text(
-                onboardingData[targetPage][1],
+                onboardingData[targetPage]['title']!,
                 style: const TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
@@ -56,12 +62,31 @@ class _OnboardingState extends State<Onboarding> {
             ),
             Padding(
               padding:
-                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 100),
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 50),
               child: Text(
-                onboardingData[targetPage][2],
+                onboardingData[targetPage]['text']!,
                 textAlign: TextAlign.center,
                 style: const TextStyle(
                   fontSize: 15,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.grey[200],
+                ),
+                width: 50,
+                height: 9,
+                alignment: Alignment.centerLeft,
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.grey[800]),
+                  width: 16.6 * (targetPage + 1),
+                  height: 8,
                 ),
               ),
             ),
@@ -78,9 +103,9 @@ class _OnboardingState extends State<Onboarding> {
                   backgroundColor: MaterialStatePropertyAll(
                       Color.fromRGBO(217, 231, 203, 1)),
                 ),
-                child: const Text(
-                  "Continue",
-                  style: TextStyle(color: Colors.black),
+                child: Text(
+                  onboardingData[targetPage]['action']!,
+                  style: const TextStyle(color: Colors.black),
                 ),
                 onPressed: () {
                   setState(() {
