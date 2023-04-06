573d8ecef9b12c46c5ce18630f6bb4165166e4b5
CedricMurairi <murairicedric@gmail.com>
2023-04-06 14:55:17 +0200

    moved to  for the onboarding screen -- little animation on navigation

--- lib/shared/onbarding_screen.dart
+++ lib/shared/onbarding_screen.dart
@@ -8,6 +8,7 @@ class Onboarding extends StatefulWidget {
 }
 
 class _OnboardingState extends State<Onboarding> {
+  PageController pageController = PageController();
   int targetPage = 0;
   final List<Map<String, String>> onboardingData = [
     {
@@ -34,92 +35,111 @@ class _OnboardingState extends State<Onboarding> {
   ];
 
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         color: Colors.white,
-        child: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20.0),
-              child: Image.asset(
-                onboardingData[targetPage]['image']!,
-              ),
+        child: PageView.builder(
+          itemCount: onboardingData.length,
+          controller: pageController,
+          onPageChanged: (value) => {
+            setState(
+              () {
+                targetPage = value;
+              },
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: Text(
-                onboardingData[targetPage]['title']!,
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+          },
+          itemBuilder: (BuildContext context, index) {
+            return Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0),
+                  child: Image.asset(
+                    onboardingData[targetPage]['image']!,
+                  ),
                 ),
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 50),
-              child: Text(
-                onboardingData[targetPage]['text']!,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 15,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.grey[200],
-                ),
-                width: 50,
-                height: 9,
-                alignment: Alignment.centerLeft,
-                child: Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.grey[800]),
-                  width: 16.6 * (targetPage + 1),
-                  height: 8,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20.0),
-              child: TextButton(
-                style: const ButtonStyle(
-                  shape: MaterialStatePropertyAll(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Text(
+                    onboardingData[targetPage]['title']!,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  fixedSize: MaterialStatePropertyAll(Size(232, 43)),
-                  backgroundColor: MaterialStatePropertyAll(
-                      Color.fromRGBO(217, 231, 203, 1)),
                 ),
-                child: Text(
-                  onboardingData[targetPage]['action']!,
-                  style: const TextStyle(color: Colors.black),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8.0, horizontal: 50),
+                  child: Text(
+                    onboardingData[targetPage]['text']!,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
                 ),
-                onPressed: () {
-                  setState(() {
-                    if (targetPage < onboardingData.length - 1) {
-                      targetPage++;
-                    } else {
-                      Navigator.pushReplacementNamed(context, '/register');
-                    }
-                  });
-                },
-              ),
-            ),
-          ],
-        )),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.grey[200],
+                    ),
+                    width: 50,
+                    height: 9,
+                    alignment: Alignment.centerLeft,
+                    child: AnimatedContainer(
+                      duration: const Duration(milliseconds: 200),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.grey[800]),
+                      width: 16.6 * (targetPage + 1),
+                      height: 8,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0),
+                  child: TextButton(
+                    style: const ButtonStyle(
+                      shape: MaterialStatePropertyAll(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                      ),
+                      fixedSize: MaterialStatePropertyAll(Size(232, 43)),
+                      backgroundColor: MaterialStatePropertyAll(
+                          Color.fromRGBO(217, 231, 203, 1)),
+                    ),
+                    child: Text(
+                      onboardingData[targetPage]['action']!,
+                      style: const TextStyle(color: Colors.black),
+                    ),
+                    onPressed: () {
+                      if (targetPage < onboardingData.length - 1) {
+                        pageController.nextPage(
+                            duration: const Duration(milliseconds: 300),
+                            curve: Curves.easeInOut);
+                      } else {
+                        Navigator.pushReplacementNamed(context, '/register');
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ));
+          },
+        ),
       ),
     );
   }
