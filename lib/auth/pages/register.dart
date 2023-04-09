b1c8bf70e9ec2aa6af8acc4b45ef7587196705df
CedricMurairi <murairicedric@gmail.com>
2023-04-09 15:58:00 +0200

    updated register page to use clean SVG assets

--- lib/auth/pages/register.dart
+++ lib/auth/pages/register.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:my_ticket/auth/layout/auth_layout.dart';
 import 'package:my_ticket/auth/widgets/auth_message.dart';
 import 'package:my_ticket/shared/rounded_styled_button.dart';
 import 'package:my_ticket/shared/text_button.dart';
+import 'package:flutter_svg/svg.dart';
 
 class Register extends StatelessWidget {
   const Register({super.key});
@@ -43,83 +45,71 @@ class Register extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        color: Colors.white,
-        child: Stack(
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(top: 40.0, left: 40.0),
-              child: Text(
-                'Register',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-              ),
+    return AuthenticationLayout(
+      leadingIsButton: false,
+      leadingText: 'Register',
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SvgPicture.asset('assets/svgs/man_on_bike.svg'),
+          RoundedStyledButton(
+            disabled: true,
+            color: const Color.fromRGBO(217, 231, 203, 1),
+            child: const Text(
+              "Continue as a Guest",
+              style: TextStyle(color: Colors.black),
             ),
-            Center(
-              child: Column(
+            action: () {
+              Navigator.pushNamed(context, '/client-home');
+            },
+          ),
+          RoundedStyledButton(
+            color: const Color.fromRGBO(255, 224, 136, 0.7),
+            child: const Text(
+              "Continue with Phone",
+              style: TextStyle(color: Colors.black),
+            ),
+            action: () {
+              Navigator.pushNamed(context, '/continue-with-phone');
+            },
+          ),
+          RoundedStyledButton(
+              border: true,
+              borderColor: Colors.black,
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Image.asset('assets/images/man_bike.png'),
-                  RoundedStyledButton(
-                    disabled: true,
-                    color: const Color.fromRGBO(217, 231, 203, 1),
-                    child: const Text(
-                      "Continue as a Guest",
+                  SvgPicture.asset('assets/svgs/gmail-icon.svg'),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 10.0),
+                    child: Text(
+                      'Continue with Google',
                       style: TextStyle(color: Colors.black),
                     ),
-                    action: () {
-                      Navigator.pushNamed(context, '/client-home');
-                    },
-                  ),
-                  RoundedStyledButton(
-                    color: const Color.fromRGBO(255, 224, 136, 0.7),
-                    child: const Text(
-                      "Continue with Phone",
-                      style: TextStyle(color: Colors.black),
-                    ),
-                    action: () {
-                      return null;
-                    },
-                  ),
-                  RoundedStyledButton(
-                      border: true,
-                      borderColor: Colors.black,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset('assets/images/gmail.png'),
-                          const Padding(
-                            padding: EdgeInsets.only(left: 10.0),
-                            child: Text(
-                              'Continue with Google',
-                              style: TextStyle(color: Colors.black),
-                            ),
-                          ),
-                        ],
-                      ),
-                      action: () async {
-                        User? user = await signInWithGoogle();
-                        if (user != null) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text('Signed as ${user.email}'),
-                            ),
-                          );
-                          Navigator.pushNamed(context, '/client-home');
-                        }
-                      }),
-                  AuthenticationMessage(
-                    message: "Already have an account?",
-                    leading: CustomTextButton(text: 'Login', action: () {Navigator.pushNamed(context, '/login');}),
                   ),
                 ],
               ),
-            ),
-          ],
-        ),
+              action: () async {
+                User? user = await signInWithGoogle();
+                if (user != null) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Signed as ${user.email}'),
+                    ),
+                  );
+                  Navigator.pushNamed(context, '/client-home');
+                }
+              }),
+          AuthenticationMessage(
+            message: "Already have an account?",
+            leading: CustomTextButton(
+                text: 'Login',
+                action: () {
+                  Navigator.pushNamed(context, '/login');
+                }),
+          ),
+        ],
       ),
     );
   }
