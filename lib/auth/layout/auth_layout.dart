d77cbe8f45816585895ffea9191f044f3b217664
CedricMurairi <murairicedric@gmail.com>
2023-04-09 15:55:49 +0200

    added general layout for the authentication flow

---
+++ lib/auth/layout/auth_layout.dart
@@ -0,0 +1,53 @@
+import "package:flutter/material.dart";
+import '../../shared/text_button.dart';
+
+class AuthenticationLayout extends StatelessWidget {
+  final bool leadingIsButton;
+  final String leadingText;
+  final String leadingRoute;
+  final Widget child;
+
+  const AuthenticationLayout(
+      {Key? key,
+      required this.child,
+      required this.leadingIsButton,
+      required this.leadingText,
+      this.leadingRoute = ""})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.white,
+        child: Stack(
+          children: [
+            leadingIsButton
+                ? Padding(
+                    padding: const EdgeInsets.only(top: 40.0, left: 40.0),
+                    child: CustomTextButton(
+                      text: leadingText,
+                      action: () {
+                        Navigator.pushNamed(context, leadingRoute);
+                      },
+                    ),
+                  )
+                : Padding(
+                    padding: const EdgeInsets.only(top: 40.0, left: 40.0),
+                    child: Text(
+                      leadingText,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 20),
+                    ),
+                  ),
+            Center(
+              child: child,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
