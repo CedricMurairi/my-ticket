6d1710983a2b1654e60532593010633a21660ffe
CedricMurairi <murairicedric@gmail.com>
2023-04-09 17:21:51 +0200

    extracted authenticator function to enable reusability

--- lib/auth/pages/register.dart
+++ lib/auth/pages/register.dart
@@ -1,47 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:my_ticket/auth/layout/auth_layout.dart';
 import 'package:my_ticket/auth/widgets/auth_message.dart';
 import 'package:my_ticket/shared/rounded_styled_button.dart';
 import 'package:my_ticket/shared/text_button.dart';
 import 'package:flutter_svg/svg.dart';
+import '../../firebase/auth.dart';
 
 class Register extends StatelessWidget {
   const Register({super.key});
-
-  Future<User?> signInWithGoogle() async {
-    User? user;
-    // Trigger the authentication flow
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-
-    // Obtain the auth details from the request
-    final GoogleSignInAuthentication? googleAuth =
-        await googleUser?.authentication;
-
-    // Create a new credential
-    final OAuthCredential credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth?.accessToken,
-      idToken: googleAuth?.idToken,
-    );
-
-    try {
-      // Once signed in, return the UserCredential
-      final UserCredential userCredential =
-          await FirebaseAuth.instance.signInWithCredential(credential);
-      // return await FirebaseAuth.instance.signInWithCredential(credential);
-      user = userCredential.user!;
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'account-exists-with-different-credential') {
-        debugPrint("The account already exists with a different credential");
-      } else if (e.code == 'invalid-credential') {
-        debugPrint("Error occurred while accessing credentials. Try again.");
-      }
-    } catch (e) {
-      debugPrint("Error occurred using Google Sign In. Try again.");
-    }
-    return user;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -91,7 +58,7 @@ class Register extends StatelessWidget {
                 ],
               ),
               action: () async {
-                User? user = await signInWithGoogle();
+                User? user = await authenticateWithGoogle();
                 if (user != null) {
                   ScaffoldMessenger.of(context).showSnackBar(
                     SnackBar(
