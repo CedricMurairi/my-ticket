72da631899bf29d60d3c5380661ac92da770548a
CedricMurairi <murairicedric@gmail.com>
2023-04-20 14:41:30 +0200

    success popup content update

--- lib/auth/widgets/success_popup.dart
+++ lib/auth/widgets/success_popup.dart
@@ -16,12 +16,20 @@ class SuccessPopUp extends StatelessWidget {
       decoration: const BoxDecoration(
           borderRadius: BorderRadius.only(
               topLeft: Radius.circular(25), topRight: Radius.circular(25))),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(message),
-          Text(description),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 55.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Icon(Icons.check_circle, color: Color(0xff48742c), size: 100,),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: Text(message, textAlign: TextAlign.center, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+            ),
+            Text(description, textAlign: TextAlign.center, style: TextStyle(fontSize: 15,fontWeight: FontWeight.w400),),
+          ],
+        ),
       ),
     );
   }
