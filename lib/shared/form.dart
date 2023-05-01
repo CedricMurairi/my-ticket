323646bf0b8442509dfb7e9d24efd9f679d04297
Adama Boubacar Oumarou <a.oumarou@alustudent.com>
2023-05-01 21:33:57 +0200

    Update

--- lib/shared/form.dart
+++ lib/shared/form.dart
@@ -7,31 +7,29 @@ class SearchForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Flex(direction: Axis.horizontal, children: [
-            TextFormField(
-              keyboardType: TextInputType.name,
-              decoration: const InputDecoration(
-                hintText: 'Name',
-                floatingLabelStyle: TextStyle(color: Colors.grey),
-                contentPadding: EdgeInsets.symmetric(horizontal: 20),
-                border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black, width: 0.5),
-                    borderRadius: BorderRadius.all(Radius.circular(25))),
-              ),
-              validator: (String? value) {
-                if (value == null) {
-                  return "Enter name";
-                }
-                return null;
-              },
-              autovalidateMode: AutovalidateMode.always,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      // ignore: prefer_const_literals_to_create_immutables
+      children: <Widget>[
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'From',
             ),
-          ]),
-        ],
-      ),
+          ),
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'To',
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
