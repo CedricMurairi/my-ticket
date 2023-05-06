1b5ac33eeeded90c12baf3dec23a1e2830074d63
CedricMurairi <murairicedric@gmail.com>
2023-05-06 17:04:32 +0200

    modified form field to show and hide country code onTap and onEditingComplete

--- lib/shared/form_field.dart
+++ lib/shared/form_field.dart
@@ -1,6 +1,6 @@
 import "package:flutter/material.dart";
 
-class CustomFormField extends StatelessWidget {
+class CustomFormField extends StatefulWidget {
   final TextEditingController controller;
   final String prefix;
   final String hint;
@@ -8,38 +8,65 @@ class CustomFormField extends StatelessWidget {
   final bool obscure;
   final double radius;
   final TextAlign alignment;
+  final bool enabled;
 
-  CustomFormField(
+  const CustomFormField(
       {required this.controller,
       required this.hint,
       required this.keyboard,
       this.obscure = false,
       this.prefix = "",
       this.radius = 25,
+      this.enabled = true,
       this.alignment = TextAlign.left,
       Key? key})
       : super(key: key);
+
+  @override
+  State<CustomFormField> createState() => _CustomFormFieldState();
+}
+
+class _CustomFormFieldState extends State<CustomFormField> {
+  bool showPrefix = false;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 5),
       child: TextFormField(
-        keyboardType: keyboard,
-        controller: controller,
-        obscureText: obscure,
-        textAlign: alignment,
+        keyboardType: widget.keyboard,
+        controller: widget.controller,
+        obscureText: widget.obscure,
+        textAlign: widget.alignment,
+        readOnly: !widget.enabled,
+        onTap: () {
+          if (widget.prefix.isNotEmpty) {
+            setState(() {
+              showPrefix = true;
+            });
+          }
+        },
+        onChanged: (value) => setState(() {
+          if (value.isNotEmpty) {
+            showPrefix = true;
+          }
+        }),
+        onEditingComplete: () => setState(() {
+          if (widget.controller.text.isEmpty) {
+            showPrefix = false;
+          }
+        }),
         decoration: InputDecoration(
-          prefixText: prefix,
-          hintText: hint,
+          prefixText: showPrefix == true ? widget.prefix : null,
+          hintText: widget.hint,
           focusedBorder: OutlineInputBorder(
               borderSide: const BorderSide(color: Colors.black, width: 0.5),
-              borderRadius: BorderRadius.all(Radius.circular(radius))),
+              borderRadius: BorderRadius.all(Radius.circular(widget.radius))),
           contentPadding:
               const EdgeInsets.symmetric(vertical: 18, horizontal: 25),
           border: OutlineInputBorder(
               borderSide: const BorderSide(color: Colors.black, width: 0.5),
-              borderRadius: BorderRadius.all(Radius.circular(radius))),
+              borderRadius: BorderRadius.all(Radius.circular(widget.radius))),
         ),
       ),
     );
