da1ce321349c2b5b568b9d9032a504068d9cfb3b
CedricMurairi <murairicedric@gmail.com>
2023-05-08 23:35:19 +0200

    modified BookingsModel to read and write bookings in localFile

--- lib/models/bookings.dart
+++ lib/models/bookings.dart
@@ -1,23 +1,61 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
 class BookingsModel extends ChangeNotifier {
   List<Map<String, dynamic>>? _bookings;
 
   List<Map<String, dynamic>>? get bookings => _bookings;
 
-  void setBookings(List<Map<String, dynamic>>? bookings) {
-    _bookings = bookings;
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> localFile() async {
+    final path = await _localPath;
+
+    return File('$path/bookings.json');
+  }
+
+  Future<File> writeBookings(String bookings) async {
+    final file = await localFile();
+
+    return file.writeAsString(bookings);
+  }
+
+  Future<List<Map<String, dynamic>>> readBookings() async {
+    try {
+      final file = await localFile();
+
+      final contents = await file.readAsString();
+
+      return List<Map<String, dynamic>>.from(json.decode(contents));
+    } catch (e) {
+      return [];
+    }
+  }
+
+  void setBookings() async {
+    _bookings = await readBookings();
+    notifyListeners();
   }
 
   void addBooking(Map<String, dynamic>? booking) {
     if (booking != null) {
       _bookings?.add(booking);
+      writeBookings(json.encode(_bookings));
+      notifyListeners();
     }
   }
 
   void deleteBooking(int? bookingId) {
     if (bookingId != null) {
       _bookings?.removeWhere((element) => element['id'] == bookingId);
+      writeBookings(json.encode(_bookings));
       notifyListeners();
     }
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import firebase_auth
 import firebase_core
 import firebase_database
 import geolocator_apple
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
