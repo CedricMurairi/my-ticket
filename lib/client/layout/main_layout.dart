4b14201f9a58f272e966146a0f05f9b421fe2182
CedricMurairi <murairicedric@gmail.com>
2023-05-08 11:40:29 +0200

    set to get location anytime MainWidgetLayout loads if the location is not in the app state

--- lib/client/layout/main_layout.dart
+++ lib/client/layout/main_layout.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:my_ticket/models/location.dart';
 import 'package:my_ticket/models/user.dart';
 import 'package:provider/provider.dart';
 
+import '../../helpers/get_location.dart';
 import '../../shared/bottom_navigation.dart';
 
 class MainLayout extends StatefulWidget {
@@ -10,27 +12,42 @@ class MainLayout extends StatefulWidget {
   const MainLayout({required this.child, Key? key}) : super(key: key);
 
   @override
-  State<MainLayout> createState() => _MainLayoutState();
+  MainLayoutState createState() => MainLayoutState();
 }
 
-class _MainLayoutState extends State<MainLayout> {
+class MainLayoutState extends State<MainLayout> {
   @override
   void initState() {
     super.initState();
+    checkCurrentUser();
+    getLocation();
+  }
 
-    if (FirebaseAuth.instance.currentUser == null) {
+  void checkCurrentUser() {
+    final currentUser = FirebaseAuth.instance.currentUser;
+    if (currentUser == null) {
       Navigator.pushNamed(context, '/login');
     } else {
-      Provider.of<UserModel>(context, listen: false)
-          .setUser(FirebaseAuth.instance.currentUser!);
+      Provider.of<UserModel>(context, listen: false).setUser(currentUser);
     }
+  }
+
+  void getLocation() {
+    if (Provider.of<LocationModel>(context, listen: false).user != null) {
+      return;
+    }
+    getUserLocation().then((value) =>
+        {Provider.of<LocationModel>(context, listen: false).setUser(value)});
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: const CustomBottomNavigationBar(),
-      body: widget.child,
+    return ChangeNotifierProvider(
+      create: (context) => LocationModel(),
+      child: Scaffold(
+        bottomNavigationBar: const CustomBottomNavigationBar(),
+        body: widget.child,
+      ),
     );
   }
 }
