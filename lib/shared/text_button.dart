7f39cfc5d66d5cff92b49d1f2b6376103ee2f95a
CedricMurairi <murairicedric@gmail.com>
2023-05-08 14:55:44 +0200

    included alignment param to align the button if necessary

--- lib/shared/text_button.dart
+++ lib/shared/text_button.dart
@@ -4,20 +4,30 @@ class CustomTextButton extends StatelessWidget {
   final String text;
   final Function action;
   final double customFontSize;
-  const CustomTextButton({Key? key, required this.text, required this.action, this.customFontSize=13}) : super(key: key);
+  final Alignment alignment;
+  final Color color;
+  const CustomTextButton({
+    Key? key,
+    required this.text,
+    required this.action,
+    this.customFontSize = 13,
+    this.color = Colors.black,
+    this.alignment = Alignment.center,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      style: ButtonStyle(
-          padding: MaterialStateProperty.all(EdgeInsets.zero),
-          overlayColor: MaterialStateProperty.all(
-              const Color.fromRGBO(217, 231, 203, 1))),
-      child: Text(text,
-          style: TextStyle(
-            fontSize: customFontSize,
-              decoration: TextDecoration.underline, color: Colors.black)),
-      onPressed: () => action()
-    );
+        style: ButtonStyle(
+            alignment: alignment,
+            padding: MaterialStateProperty.all(EdgeInsets.zero),
+            overlayColor: MaterialStateProperty.all(
+                const Color.fromRGBO(217, 231, 203, 0))),
+        child: Text(text,
+            style: TextStyle(
+                fontSize: customFontSize,
+                decoration: TextDecoration.underline,
+                color: color)),
+        onPressed: () => action());
   }
 }
