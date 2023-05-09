236f8dd8d9ec6dc3d4b2e6e9a9bfecaaa7e8cda3
CedricMurairi <murairicedric@gmail.com>
2023-05-09 19:59:32 +0200

    stop returning User and kept the function to just auth

--- lib/firebase/auth.dart
+++ lib/firebase/auth.dart
@@ -2,8 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-Future<User?> authenticateWithGoogle() async {
-  User? user;
+Future<void> authenticateWithGoogle() async {
   // Trigger the authentication flow
   final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
@@ -18,11 +17,7 @@ Future<User?> authenticateWithGoogle() async {
   );
 
   try {
-    // Once signed in, return the UserCredential
-    final UserCredential userCredential =
-        await FirebaseAuth.instance.signInWithCredential(credential);
-    // return await FirebaseAuth.instance.signInWithCredential(credential);
-    user = userCredential.user!;
+    await FirebaseAuth.instance.signInWithCredential(credential);
   } on FirebaseAuthException catch (e) {
     if (e.code == 'account-exists-with-different-credential') {
       debugPrint("The account already exists with a different credential");
@@ -32,5 +27,4 @@ Future<User?> authenticateWithGoogle() async {
   } catch (e) {
     debugPrint("Error occurred using Google Sign In. Try again.");
   }
-  return user;
 }
